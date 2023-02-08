7134574b274615cf34e97b0f7ee20c44a907b9ff
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-08 05:21:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,33 +4,11 @@ import {
   createRoutesFromElements,
   Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Products from "./pages/Products";
-import RootLayout from "./components/Root";
-import ErrorPage from './components/Error';
-import ProductDetails from "./components/ProductDetails";
 
-// const routeDefinitions = createRoutesFromElements(
-//   <Route>
-//     <Route path='/' element={<Home />}/>
-//     <Route path='/products' element={<Products />}/>
-//   </Route>
-// );
-const router = createBrowserRouter([
-  {
-    path: '/', 
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {path: '/', element: <Home />},
-      {path: '/products', element: <Products />},
-      {path: '/products/:productId', element: <ProductDetails/>} 
-    ]
-  },
-]);
 
-// const router = createBrowserRouter(routeDefinitions);
+
 const App = () => {
  
-  return <RouterProvider router={router}/>
+  return <h1>Hi</h1>
 };
 export default App;
